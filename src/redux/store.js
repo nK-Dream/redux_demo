0f96113ba501273575dbff1ae0fdf9a4026b4066
J <oncwnuAtbq7RWXrDbsrMJw4vPdjA@git.weixin.qq.com>
2021-07-09 17:46:59 +0800

    更新

--- src/redux/store.js
+++ src/redux/store.js
@@ -10,6 +10,8 @@ import countReducer from './reducers/count'
 import pensonReducer from './reducers/person'
 //引入用于支持异步action的redux-thunk中间件
 import thunk from 'redux-thunk'
+//引入composeWithDevTools用于支持开发者工具的使用
+import {composeWithDevTools} from 'redux-devtools-extension'
 
 const allReducer = combineReducers({
     countReducer,
